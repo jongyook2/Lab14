02d0c1da7d789000f7c6f862efc65379f601e04b
Yoon Jong Wook <emrrn92@naver.com>
2018-05-10 12:29:40 +0900

    add S.L.comment in prob1

--- src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimiter.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimiter.java
@@ -1,42 +1,42 @@
-package edu.handong.csee.java.lab14.prob1;
-import java.util.Scanner;
-import java.util.Random;
+package edu.handong.csee.java.lab14.prob1; //package name
+import java.util.Scanner; //import standard input from keyboard
+import java.util.Random; //import Random util(generate random)
 
-public class SpeedLimiter {
-	int speedLimit;
-	int yourSpeed;
-	public int getSpeedLimit() {
-		return speedLimit;
+public class SpeedLimiter { //declare class
+	int speedLimit; //declare instance variable(int speedLimit)
+	int yourSpeed; //declare instance variable(int yourSpeed)
+	public int getSpeedLimit() { //declare getter of speedLimit
+		return speedLimit; //return speedLimit
 	}
-	public void setSpeedLimit(int speedLimit) {
-		this.speedLimit = speedLimit;
+	public void setSpeedLimit(int speedLimit) { //declare setter of speedLimit
+		this.speedLimit = speedLimit; //store speedLimit in this.speedLimit
 	}
-	public int getYourSpeed() {
-		return yourSpeed;
+	public int getYourSpeed() { //declare getter of YourSpeed
+		return yourSpeed; //return yourSpeed
 	}
-	public void setYourSpeed(int yourSpeed) {
-		this.yourSpeed = yourSpeed;
+	public void setYourSpeed(int yourSpeed) { //declare setter of speedLimit
+		this.yourSpeed = yourSpeed; //store yourSpeed in this.yourSpeed
 	}
 
-	public SpeedLimiter() {
-		Scanner keyboard = new Scanner(System.in);
-		Random Random = new Random();
-		System.out.print("Set the speed limit, officer: ");
-		speedLimit = keyboard.nextInt();
-		yourSpeed = Random.nextInt(101);
+	public SpeedLimiter() { //Constructor
+		Scanner keyboard = new Scanner(System.in); // instantiate Class Scanner to keyboard(input)
+		Random random = new Random(); //instantiate Class Random to random
+		System.out.print("Set the speed limit, officer: "); //print(Set the speed limit, officer:)
+		speedLimit = keyboard.nextInt();//store input(keyboard) in speedLimit
+		yourSpeed = random.nextInt(101); //store random number(0~100) in yourSpeed
 	}
-	public static void warnSpeedLimit(int speedLimit, int yourSpeed) {
-		try {
-			if(speedLimit < yourSpeed) {
-				throw new Exception("Speed Limit "+speedLimit+"km exceeded!");
+	public static void warnSpeedLimit(int speedLimit, int yourSpeed) { //declare warn method(speed over)
+		try { //start
+			if(speedLimit < yourSpeed) { //if speedLimit is lower than yourSpeed
+				throw new Exception("Speed Limit "+speedLimit+"km exceeded!"); //throw new Exception and print
 			}
-			System.out.println("you are a law abiding citizen!");
+			System.out.println("you are a law abiding citizen!");//print(you are a law abiding citizen!)
 		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("You are being fined");
+		catch(Exception e) { //if it catches exception
+			System.out.println(e.getMessage()); //print exception's message
+			System.out.println("You are being fined");//print(You are being fined)
 		}
-		System.out.println("Your current speed: "+ yourSpeed);
+		System.out.println("Your current speed: "+ yourSpeed);//print(Your current speed:)
 	}
 }
 
--- src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimiterMain.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimiterMain.java
@@ -1,8 +1,8 @@
-package edu.handong.csee.java.lab14.prob1;
+package edu.handong.csee.java.lab14.prob1; //package name
 
-public class SpeedLimiterMain {
-	public static void main(String[] args) {
-		SpeedLimiter speedLimiter = new SpeedLimiter();
-		SpeedLimiter.warnSpeedLimit(speedLimiter.getSpeedLimit(), speedLimiter.getYourSpeed());		
+public class SpeedLimiterMain { //declare SpeedLimiterMain class
+	public static void main(String[] args) { //declare main method
+		SpeedLimiter speedLimiter = new SpeedLimiter(); //instantiate speedLimiter class to speedLimiter(instance)
+		SpeedLimiter.warnSpeedLimit(speedLimiter.getSpeedLimit(), speedLimiter.getYourSpeed()); //call warnSpeedLimit method
 	}
 }
